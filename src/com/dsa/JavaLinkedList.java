a302ffe1e513412a7ec8e97fc67e950e9d7c910c
james <jimmindungu3@gmail.com>
2025-07-29 08:19:48 +0300

    LinkedList

---
+++ src/com/dsa/JavaLinkedList.java
@@ -0,0 +1,24 @@
+package com.dsa;
+
+import java.util.LinkedList;
+
+public class JavaLinkedList {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+
+        System.out.println(cars.getFirst()); // Volvo
+        System.out.println(cars.getLast()); // Ford
+        System.out.println(cars); // [Volvo, BMW, Ford]
+        cars.addFirst("Mazda");
+        System.out.println(cars); // [Mazda, Volvo, BMW, Ford]
+        cars.addLast("Suzuki");
+        System.out.println(cars); // [Mazda, Volvo, BMW, Ford, Suzuki]
+        cars.removeLast();
+        System.out.println(cars); // [Mazda, Volvo, BMW, Ford]
+        cars.removeFirst();
+        System.out.println(cars); // [Volvo, BMW, Ford]
+    }
+}
